d6b373b19d952afff02a996e15c63e5735820948
Chris McDonald <cmacweb1@gmail.com>
2018-01-13 15:37:10 -0500

    add debug line in user_exists.sh

--- tests/user_exists.sh
+++ tests/user_exists.sh
@@ -4,6 +4,7 @@
 set -e 
 
 docker exec --tty ${container_id} env TERM=xterm user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
+echo "TESTING!!!"
 echo "$user_exists"
 if [ $user_exists -eq 0 ]; then 
   echo "User exists"
