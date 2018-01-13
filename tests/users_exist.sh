6e833a92e748639ba14c51758ad3ccfd1dca138f
Chris McDonald <cmacweb1@gmail.com>
2018-01-13 16:00:38 -0500

    cleanup and rename user_exists.sh and cleanup and edit travis file to have script point to the renamed script file

--- tests/users_exist.sh
+++ tests/users_exist.sh
@@ -3,19 +3,16 @@
 # Exit on any command failure
 set -e 
 
-# docker exec --tty ${container_id} env TERM=xterm user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
 deploy_test_1_user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
-echo "TESTING!!!"
-echo "$user_exists"
 if [ $deploy_test_1_user_exists -eq 0 ]; then 
-  echo "User exists"
+  echo "User deploy_test_1 exists"
 else
-  echo "User does not exist"
+  echo "User deploy_test_1 does not exist"
 fi
 
 deploy_test_2_user_exists=$(id -u deploy_test_2 > /dev/null 2>&1; echo $?);
 if [ $deploy_test_2_user_exists -eq 0 ]; then 
-  echo "User exists"
+  echo "User deploy_test_2 exists"
 else
-  echo "User does not exist"
+  echo "User deploy_test_2 does not exist"
 fi
