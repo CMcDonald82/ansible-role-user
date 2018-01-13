5b2b3f1a7499c6acdbd120afe41135ad049d9580
Chris McDonald <cmacweb1@gmail.com>
2018-01-13 15:55:29 -0500

    add test for deploy_test_2 user to user_exists.sh

--- tests/user_exists.sh
+++ tests/user_exists.sh
@@ -4,13 +4,18 @@
 set -e 
 
 # docker exec --tty ${container_id} env TERM=xterm user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
-user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
+deploy_test_1_user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
 echo "TESTING!!!"
 echo "$user_exists"
-if [ $user_exists -eq 0 ]; then 
+if [ $deploy_test_1_user_exists -eq 0 ]; then 
   echo "User exists"
 else
   echo "User does not exist"
 fi
 
-# echo $user_exists
+deploy_test_2_user_exists=$(id -u deploy_test_2 > /dev/null 2>&1; echo $?);
+if [ $deploy_test_2_user_exists -eq 0 ]; then 
+  echo "User exists"
+else
+  echo "User does not exist"
+fi
