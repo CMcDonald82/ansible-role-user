4fab55c16d45c9e73d37544b1c1b426e267caffe
Chris McDonald <cmacweb1@gmail.com>
2018-01-13 15:42:37 -0500

    try running user_exists.sh script from within Docker container

--- tests/user_exists.sh
+++ tests/user_exists.sh
@@ -3,7 +3,8 @@
 # Exit on any command failure
 set -e 
 
-docker exec --tty ${container_id} env TERM=xterm user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
+# docker exec --tty ${container_id} env TERM=xterm user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
+user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
 echo "TESTING!!!"
 echo "$user_exists"
 if [ $user_exists -eq 0 ]; then 
