e356601c3473a67ac08a571764ccf5db219ef655
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-14 12:08:05 -0500

    cleanup users_exist.sh file

--- tests/users_exist.sh
+++ tests/users_exist.sh
@@ -6,8 +6,7 @@ set -e
 user_list=(deploy_test_1 deploy_test_2)
 for usr in ${user_list[*]}
 do
-  user_exists=$(id -u "$usr" > /dev/null 2>&1; echo $?);
-  echo "$user_exists"
+  user_exists=$(id -u "$usr" > /dev/null 2>&1; echo $?)
   if [ $user_exists -eq 0 ]; then 
     echo "User $usr exists"
   else
@@ -15,16 +14,3 @@ do
   fi
 done
 
-# deploy_test_1_user_exists=$(id -u deploy_test_1 > /dev/null 2>&1; echo $?);
-# if [ $deploy_test_1_user_exists -eq 0 ]; then 
-#   echo "User deploy_test_1 exists"
-# else
-#   echo "User deploy_test_1 does not exist"
-# fi
-
-# deploy_test_2_user_exists=$(id -u deploy_test_2 > /dev/null 2>&1; echo $?);
-# if [ $deploy_test_2_user_exists -eq 0 ]; then 
-#   echo "User deploy_test_2 exists"
-# else
-#   echo "User deploy_test_2 does not exist"
-# fi
