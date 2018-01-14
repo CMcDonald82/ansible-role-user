b5e1064fd2a83293330f52e95dd98b46a84bc4c9
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-14 13:20:23 -0500

    debugging users_sudo.sh 9th time

--- tests/users_sudo.sh
+++ tests/users_sudo.sh
@@ -6,7 +6,7 @@ set -e
 user_list=(deploy_test_1 deploy_test_3)
 for usr in ${user_list[*]}
 do
-  sudo_enabled=$(sudo -n -v 2>&1); test -z "$sudo_enabled" || echo $sudo_enabled | grep -q password
+  sudo_enabled=$(sudo -n -v 2>&1); #test -z "$sudo_enabled" || echo $sudo_enabled | grep -q password
   echo "$sudo_enabled"
   # sudo -n -l -U "$usr" 2>&1
 done
