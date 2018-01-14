114c01ff218e8d7105279e2319a187de47d7caca
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-14 11:49:12 -0500

    try fixing syntax error in users_exist.sh 3rd time

--- tests/users_exist.sh
+++ tests/users_exist.sh
@@ -5,7 +5,7 @@ set -e
 
 user_list='deploy_test_1 deploy_test_2'
 for usr in $user_list do
-  user_exists=$(id -u ${usr} > /dev/null 2>&1; echo $?);
+  user_exists=$(id -u "${usr}" > /dev/null 2>&1; echo $?);
   if [ $user_exists -eq 0 ]; then 
     echo "User $usr exists"
   else
