4155ec05b897e0bd99e96beb207a7e578e798800
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-15 11:32:22 -0500

    debugging users_sudo.sh 19th time

--- tests/users_sudo.sh
+++ tests/users_sudo.sh
@@ -9,10 +9,7 @@ do
   # test sudo -l -U "$usr" 2>&1
   # sudo_enabled=$(sudo -l -U "$usr" 2>&1) | grep '(ALL) NOPASSWD: ALL'; #test -z "$sudo_enabled" || echo $sudo_enabled | grep -q password
   # echo $?
-  sudo groups $usr 
-  | grep -q "sudo"
-  && (echo "Group: PASS" && exit 0)
-  || (echo "Group: FAIL" && exit 0)
+  sudo groups $usr | grep -q "sudo" && (echo "Group: PASS" && exit 0) || (echo "Group: FAIL" && exit 0)
   # if [ $? -eq 1]
   # then
   #   echo "NOT!!!"
