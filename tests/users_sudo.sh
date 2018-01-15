dba58d3776e16950ea264a4033483d4d870006e2
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-01-15 11:27:21 -0500

    debugging users_sudo.sh 17th time

--- tests/users_sudo.sh
+++ tests/users_sudo.sh
@@ -7,13 +7,15 @@ user_list=(deploy_test_1 deploy_test_3)
 for usr in ${user_list[*]}
 do
   # test sudo -l -U "$usr" 2>&1
-  sudo_enabled=$(sudo -l -U "$usr" 2>&1) | grep '(ALL) NOPASSWD: ALL'; #test -z "$sudo_enabled" || echo $sudo_enabled | grep -q password
+  # sudo_enabled=$(sudo -l -U "$usr" 2>&1) | grep '(ALL) NOPASSWD: ALL'; #test -z "$sudo_enabled" || echo $sudo_enabled | grep -q password
   # echo $?
-  
-  if [ $? -eq 1]
-  then
-    echo "NOT!!!"
-  fi
-  echo "$sudo_enabled"
+  sudo groups $usr | grep -q "sudo"
+  && (echo "Group: PASS" && exit 0)
+  || (echo "Group: FAIL" && exit 0)
+  # if [ $? -eq 1]
+  # then
+  #   echo "NOT!!!"
+  # fi
+  # echo "$sudo_enabled"
   # sudo -n -l -U "$usr" 2>&1
 done
